a23d182489f7205112e83ce27fd5f7eb0664f6da
Karan Khandelwal <karan@zozuk.com>
2024-05-23 00:38:47 +0530

    Tried and tested

--- practice.js
+++ practice.js
@@ -6,48 +6,64 @@ class Cell {
     }
 }
 
+const minefield = [];
+
 function createBoard(width, height) {
     const gridWidth = width;
     const gridHeight = height;
-    const minefield = [];
     for (let x = 0; x < gridWidth; x++) {
         minefield[x] = [];
         for (let y = 0; y < gridHeight; y++) {
             minefield[x][y] = new Cell(x, y);
         }
     }
+    return minefield;
+}
 
-    function getAdjacentCells(cell, minefield) {
-        const adjacentCells = [];
-        const gridWidth = minefield.length;
-        const gridHeight = minefield[0].length;
-    
-        for (let dx = -1; dx <= 1; dx++) {
-            for (let dy = -1; dy <= 1; dy++) {
-                if (dx === 0 && dy === 0) continue;
-    
-                const newX = cell.x + dx;
-                const newY = cell.y + dy;
-    
-                if (newX >= 0 && newX < gridWidth && newY >= 0 && newY < gridHeight) {
-                    adjacentCells.push(minefield[newX][newY]);
-                }
-            }
-        }
-    
-        return adjacentCells;
-    }
+function getAdjacentCells(cell, minefield) {
+    const adjacentCells = [];
+    const gridWidth = minefield.length;
+    const gridHeight = minefield[0].length;
 
-    function placeMines(numberOfMines) {
-        let minesPlaced = 0;
-    
-        while (minesPlaced < numberOfMines) {
-            const x = Math.floor(Math.random() * minefield.length);
-            const y = Math.floor(Math.random() * minefield[0].length);
-    
-            if (!minefield[x][y].isMine) {
-                minefield[x][y].isMine = true;
-                minesPlaced++;
+    for (let dx = -1; dx <= 1; dx++) {
+        for (let dy = -1; dy <= 1; dy++) {
+            if (dx === 0 && dy === 0) continue;
+
+            const newX = cell.x + dx;
+            const newY = cell.y + dy;
+            
+            if (newX >= 0 && newX < gridWidth && newY >= 0 && newY < gridHeight) {
+                adjacentCells.push(minefield[newX][newY]);
             }
         }
     }
+
+    return adjacentCells;
+}
+
+function placeMines(numberOfMines) {
+    let minesPlaced = 0;
+
+    while (minesPlaced < numberOfMines) {
+        const x = Math.floor(Math.random() * minefield.length);
+        const y = Math.floor(Math.random() * minefield[0].length);
+
+        if (!minefield[x][y].isMine) {
+            minefield[x][y].isMine = true;
+            minesPlaced++;
+        }
+    }
+    return minesPlaced;
+}
+
+createBoard(3, 3);
+
+const exampleCell = minefield[1][1];
+
+
+const adjacentCells = getAdjacentCells(exampleCell, minefield);
+// console.log(adjacentCells);
+
+
+placeMines(7);
+console.log(minefield);
