d1a2e4f7cc0803af11fb3fd51b1800f290e52851
Karan Khandelwal <karan@zozuk.com>
2024-05-23 10:44:08 +0530

    getAdjacentMines

--- practice.js
+++ practice.js
@@ -31,7 +31,7 @@ function getAdjacentCells(cell, minefield) {
 
             const newX = cell.x + dx;
             const newY = cell.y + dy;
-            
+
             if (newX >= 0 && newX < gridWidth && newY >= 0 && newY < gridHeight) {
                 adjacentCells.push(minefield[newX][newY]);
             }
@@ -56,14 +56,27 @@ function placeMines(numberOfMines) {
     return minesPlaced;
 }
 
+
+function getAdjacentMines(cell, minefield) {
+    const adjacentCells = getAdjacentCells(cell, minefield);
+    let mineCount = 0;
+    for (const adjacentCell of adjacentCells) {
+        if (adjacentCell.isMine) {
+            mineCount++;
+        }
+    }
+    return mineCount;
+}
+
 createBoard(3, 3);
+placeMines(7);
 
 const exampleCell = minefield[1][1];
 
+const adjacentmines = getAdjacentMines(exampleCell, minefield);
+// console.log(adjacentmines);
 
 const adjacentCells = getAdjacentCells(exampleCell, minefield);
 // console.log(adjacentCells);
 
-
-placeMines(7);
-console.log(minefield);
+console.log(minefield)
